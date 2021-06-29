66c7407db420e9eb4e2ddc610162c2b7062f4d46
Coeat.k <coeat.k@kakaocorp.com>
2021-06-29 14:09:20 +0900

    feat: changing

--- algo/src/main/kotlin/Main.kt
+++ algo/src/main/kotlin/Main.kt
@@ -1,27 +1,41 @@
+import kotlin.collections.*
 import kotlin.io.*
 import kotlin.text.*
 
 /*
- * Complete the 'camelcase' function below.
+ * Complete the 'pangrams' function below.
  *
- * The function is expected to return an INTEGER.
+ * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
 
-fun camelcase(s: String): Int {
-    var cnt = 1
+fun pangrams(s: String): String {
+    val map = HashMap<Char, Boolean>()
+    var result = 0
+    for (ch in 'a'..'z') {
+        map[ch] = false
+    }
     for (ch in s) {
-        if (ch in 'A'..'Z') {
-            cnt++
+        if(ch.toLowerCase() in 'a'..'z'){
+            map[ch.toLowerCase()] = true
         }
     }
-    return cnt
+    for (atom in map) {
+        if (atom.value) {
+            result++
+        }
+    }
+    return if (result == 26) {
+        "pangram"
+    } else {
+        "not pangram"
+    }
 }
 
 fun main(args: Array<String>) {
     val s = readLine()!!
 
-    val result = camelcase(s)
+    val result = pangrams(s)
 
     println(result)
 }
